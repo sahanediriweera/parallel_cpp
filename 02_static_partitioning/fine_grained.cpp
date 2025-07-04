c8b53d2d3a2097d86751acb7a976f59c7a3f00e2
sahanediriweera <rsahanediriweera@gmail.com>
2025-07-04 07:51:14 +0530

    feat:add-static-distribution-fine_grained

---
+++ 02_static_partitioning/fine_grained.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <chrono>
+#include <iterator>
+#include <random>
+#include <span>
+#include <thread>
+#include <vector>
+
+int main()
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+
+    std::uniform_int_distribution bin_1(1, 25);
+    std::uniform_int_distribution bin_2(26, 50);
+    std::uniform_int_distribution bin_3(51, 75);
+    std::uniform_int_distribution bin_4(76, 100);
+
+    int num_work_items = 1 << 10;
+    int n_bins = 4;
+    int elements_per_bin = num_work_items / n_bins;
+
+    std::vector<int> work_items;
+
+    std::generate_n(
+        std::back_inserter(work_items),
+        elements_per_bin,
+        [&]
+        { return bin_1(mt); });
+    std::generate_n(
+        std::back_inserter(work_items),
+        elements_per_bin,
+        [&]
+        { return bin_2(mt); });
+    std::generate_n(
+        std::back_inserter(work_items),
+        elements_per_bin,
+        [&]
+        { return bin_3(mt); });
+    std::generate_n(
+        std::back_inserter(work_items),
+        elements_per_bin,
+        [&]
+        { return bin_4(mt); });
+
+    int num_threads = 0;
+
+    auto work = [&](int thread_id)
+    {
+        for (int i = thread_id; i < num_work_items; i++)
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(work_items[i]));
+        }
+    };
+
+    std::vector<std::jthread> threads;
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        threads.emplace_back(work, i);
+    }
+
+    return 0;
+}
